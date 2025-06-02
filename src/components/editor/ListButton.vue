b71bf27bcd5ed6a46e6c8f5265629614e480846b
g.okhrimenko <sunrisehype@gmail.com>
2025-06-02 09:26:57 +0200

    feat: list buttons

--- src/components/editor/ListButton.vue
+++ src/components/editor/ListButton.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-type ListType = 'bulletList' | 'orderedList' | 'taskList';
-</script>
+import { storeToRefs } from 'pinia';
 
+import { useEditorStore } from '@/stores/editor';
+
+type ListType = 'bulletList' | 'orderedList' | 'taskList';
+
+const props = defineProps<{
+  listType: ListType;
+}>();
+
+const { editor } = storeToRefs(useEditorStore());
+</script>
 
 <template>
   <div class="flex items-center gap-2">
-
+    {{ listType }}
   </div>
 </template>
-
--- src/components/editor/ListDropdownMenu.vue
+++ src/components/editor/ListDropdownMenu.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { useEditorStore } from '@/stores/editor';
+import { computed, type Component } from 'vue';
 import { storeToRefs } from 'pinia';
+
+import { useEditorStore } from '@/stores/editor';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,17 +11,17 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Toggle } from '@/components/ui/toggle';
+
 import { ListIcon, ChevronDownIcon, ListOrderedIcon, ListTodoIcon } from 'lucide-vue-next';
-import { computed, type Component } from 'vue';
 
 type ListOption = {
   icon: Component;
   label: string;
-  value: ListType;
+  value: 'bulletList' | 'orderedList' | 'taskList';
 };
 
 type ListProps = {
-  types: ['bulletList', 'orderedList', 'taskList'];
+  types: ('bulletList' | 'orderedList' | 'taskList')[];
 };
 
 const listOptions: ListOption[] = [
@@ -47,13 +50,31 @@ const { editor } = storeToRefs(store);
 
 const canToggleList = computed(() => {
   if (!editor.value) return false;
+
+  return true;
 });
+
+const filteredOptions = computed(() =>
+  listOptions.filter((option) => props.types.includes(option.value)),
+);
+
+console.log(filteredOptions.value);
+
+const toggleList = (listType: ListOption['value']) => {
+  if (!editor.value) return;
+
+  if (listType === 'taskList') {
+    editor.value.chain().focus().toggleTaskList().run();
+  } else {
+    editor.value.chain().focus().toggleList(listType, 'listItem').run();
+  }
+};
 </script>
 
 <template>
   <DropdownMenu>
     <DropdownMenuTrigger as-child>
-      <Toggle>
+      <Toggle :disabled="!canToggleList">
         <div class="flex items-center gap-1">
           <ListIcon class="h-4 w-4" />
           <ChevronDownIcon class="size-3" />
@@ -61,8 +82,19 @@ const canToggleList = computed(() => {
       </Toggle>
     </DropdownMenuTrigger>
     <DropdownMenuContent>
-      <DropdownMenuItem v-for="option in listOptions" :key="option.value" as-child>
-        <ListButton :list-type="option.value" :editor="editor" />
+      <DropdownMenuItem v-for="option in filteredOptions" :key="option.value" as-child>
+        <!-- <ListButton :list-type="option.value" /> -->
+        <Toggle
+          :data-state="editor?.isActive(option.value) ? 'on' : 'off'"
+          :state="editor?.isActive(option.value) ? 'on' : 'off'"
+          @click="toggleList(option.value)"
+          class="w-full justify-between"
+        >
+          <div class="flex items-center gap-2">
+            <component :is="option.icon" class="h-4 w-4" />
+            <span>{{ option.label }}</span>
+          </div>
+        </Toggle>
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
